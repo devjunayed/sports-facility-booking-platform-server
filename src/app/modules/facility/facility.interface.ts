7be17f2c05a93511390c1c1dba75b11fceb1367e
Md Junayed <devjunayed@gmail.com>
2024-09-10 13:04:46 +0600

    facility interface and model is created

---
+++ src/app/modules/facility/facility.interface.ts
@@ -0,0 +1,7 @@
+export type TFacility = {
+    name: string;
+    description: string;
+    pricePerHour: number;
+    location: string;
+    isDeleted: boolean;
+}
---
+++ src/app/modules/facility/facility.model.ts
@@ -0,0 +1,27 @@
+import { model, Schema } from 'mongoose'
+import { TFacility } from './facility.interface'
+
+const facilitySchema = new Schema<TFacility>({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  pricePerHour: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+export const FacilityModel = model<TFacility>('Facility', facilitySchema)
